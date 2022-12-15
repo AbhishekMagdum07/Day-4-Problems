f38727b13d7080f1d43ce3da9edafd078bd86bec
Abhishek <abhishek.magdum.07@gmail.com>
2022-12-15 11:06:51 +0530

    Number of times dice was played

--- src/com/snakeladder/Uc6_SnakeAndLadder.java
+++ src/com/snakeladder/Uc6_SnakeAndLadder.java
@@ -2,13 +2,15 @@ package com.snakeladder;
 
 import java.util.Random;
 
-public class Uc4_Uc5SnakeAndLadder {
+public class Uc6_SnakeAndLadder {
     public static void main(String[] args) {
 
         int playerPosition = 0;
+        int count = 0;
         Random random = new Random();
 
         while (playerPosition < 100) {
+            count ++;
             int diceNumber = random.nextInt(6) + 1;
             int move = random.nextInt(3);
 
@@ -20,20 +22,22 @@ public class Uc4_Uc5SnakeAndLadder {
                 case 1:
                     System.out.println("Ladder");
                     playerPosition = playerPosition + diceNumber;
-                    if(playerPosition > 100) {
+                    if (playerPosition > 100) {
                         playerPosition = 100;
                     }
                     break;
                 default:
                     System.out.println("Snake");
                     playerPosition = playerPosition - diceNumber;
-                    if(playerPosition < 0){
+                    if (playerPosition < 0) {
                         playerPosition = 0;
                     }
                     break;
             }
 
             System.out.println("Player position is " + playerPosition);
+
         }
+        System.out.println("Number of Times dice was played to win the game = " + count);
     }
 }
