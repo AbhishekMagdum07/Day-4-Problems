750a564def27d85a5a1a69071729f262ddffe19c
Abhishek <abhishek.magdum.07@gmail.com>
2022-12-15 10:08:07 +0530

    first commit

---
+++ src/com/snakeladder/Uc1_SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snakeladder;
+
+
+
+public class Uc1_SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Game Start at position = 0");
+    }
+
+}
